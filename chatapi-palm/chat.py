dafd2a37f0659863b335d68efbe3c80d6da41125
shin5ok <shin5ok@55mp.com>
2023-08-28 20:01:48 +0900

    generalize by taking params from env

--- chatapi-palm/chat.py
+++ chatapi-palm/chat.py
@@ -1,20 +1,26 @@
+from typing import Union, Dict, Optional
 import vertexai
 from vertexai.preview.language_models import ChatModel, InputOutputTextPair
 
-# Project IDを指定してください
-project_id = "<Project ID>"
-location = "us-central1"
+class Chat:
 
-vertexai.init(project=project_id, location=location)
-chat_model = ChatModel.from_pretrained("chat-bison@001")
-parameters = {
-    "temperature": 0.2,
-    "max_output_tokens": 1024,
-    "top_p": 0.8,
-    "top_k": 40
-}
+    def __init__(self, 
+                    project_id: Optional[str], 
+                    model_location: Optional[str], 
+                    params: Dict[str, Union[int,float]] = {}
+                ):
 
-def chat(req):
-    chat = chat_model.start_chat()
-    response = chat.send_message(req, **parameters)
-    return response
+        vertexai.init(project=project_id, location=model_location)
+        self.chat_model = ChatModel.from_pretrained("chat-bison@001")
+        self.parameters = {
+            "temperature": 0.2,
+            "max_output_tokens": 1024,
+            "top_p": 0.8,
+            "top_k": 40
+        }
+        self.parameters.update(params)
+
+    def chat(self, req: str):
+        chat = self.chat_model.start_chat()
+        response = chat.send_message(req, **self.parameters)
+        return response
--- chatapi-palm/main.py
+++ chatapi-palm/main.py
@@ -1,15 +1,23 @@
 from typing import Any, Mapping
 from verify_token import verify_id_token
-from chat import chat
+from chat import Chat
 import os
 import flask
 import functions_framework
+
+e = os.environ
+SKIP_VERIFICATION = e.get("SKIP_VERIFICATION")
+PROJECT_ID = e.get("GOOGLE_CLOUD_PROJECT")
+PROJECT_NUMBER = e.get("PROJECT_NUMBER")
+MODEL_LOCATION = e.get("MODEL_LOCATION", "us-central1")
+
+chat = Chat(PROJECT_ID, MODEL_LOCATION)
 
 @functions_framework.http
 
 def hello_chat(req: flask.Request) -> Mapping[str, Any]:
   
-  if os.environ.get("SKIP_VERIFICATION", "False") != "True":
+  if SKIP_VERIFICATION != "True":
     # ヘッダーよりAuthorizationを取得
     auth_header = flask.request.headers.get('Authorization')
 
@@ -21,7 +29,7 @@ def hello_chat(req: flask.Request) -> Mapping[str, Any]:
         return flask.abort(403)
 
     # tokenの検証
-    verify_id_token(bearer_token)
+    verify_id_token(PROJECT_NUMBER, bearer_token)
   
   request_json = req.get_json(silent=True)
   if request_json:
@@ -30,7 +38,7 @@ def hello_chat(req: flask.Request) -> Mapping[str, Any]:
     text = "Hi!"
 
   # PaLM API実行
-  res = chat(text)
+  res = chat.chat(text)
 
   # 応答をChat形式に変更
   chat_response = {
