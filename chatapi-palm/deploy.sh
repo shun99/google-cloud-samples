827a5a4aac2bcc7862346b71d7abe15a9ef09ed2
shin5ok <shin5ok@55mp.com>
2023-08-29 17:40:05 +0900

    fix missing one

--- chatapi-palm/deploy.sh
+++ chatapi-palm/deploy.sh
@@ -3,7 +3,7 @@ PROJECT_ID=$(gcloud config get project)
 PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format="value(projectNumber)")
 
 gcloud functions deploy \
-    test-chat \
+    ${NAME} \
     --gen2 \
     --runtime=python311 \
     --region=asia-northeast1 \
